e96a4c48dd017cd1afb47a22add173295ceb54fa
Natalie-codeNovice <anathaliemukash@gmail.com>
2025-12-01 13:01:10 +0200

    Initial commit: Add basic Node.js web app

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+res.writeHead(200, { 'Content-Type': 'text/plain' });
+res.end('Hello, this is a basic web app!');
+});
+server.listen(3000, () => {
+console.log('Server running on http://localhost:3000');
+});
